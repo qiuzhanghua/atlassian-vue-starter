7131b052f8b13b42e32038a73f2d038b74acc069
qiuzhanghua <qiuzhanghua@icloud.com>
2024-04-16 06:43:01 +0800

    feat: mix 2 methods of design tokens

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { token, setGlobalTheme } from '@atlaskit/tokens';
 setGlobalTheme({light: "light", dark: "legacy-dark", colorMode: 'auto'});
 
-console.log(token('color.background.danger'))
 let color = token('color.text.accent.blue');
 let styleObject = {color};
 
@@ -10,8 +9,12 @@ let styleObject = {color};
 
 <template>
   <div id="app" :style="{'color': token('color.text.accent.red')}">Hello</div>
-  <div id="app" :style="styleObject">Again</div>
+  <div id="app2" :style="styleObject">Again</div>
+  <div id="app3" :style="{'letter-spacing': token('space.800')}">Third</div>
 </template>
 
 <style scoped>
+#app3 {
+  color: var(--ds-text-accent-green);
+}
 </style>
